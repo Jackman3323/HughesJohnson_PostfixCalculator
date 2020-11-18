4365c42bde4871c4dd84a0d13a25d2d5faa414b9
Jackman3323 <jhughes22@kentdenver.org>
2020-11-18 09:54:49 -0700

    I fixed all of the bugs. I finished the driver. The only thing remaining is to read from a text file.

--- src/PostFixCalculatorDriver.java
+++ src/PostFixCalculatorDriver.java
@@ -1,6 +1,6 @@
 public class PostFixCalculatorDriver {
     public static void main(String[] args) {
         CommunistCalculator c = new CommunistCalculator();
-        c.math("5 6 7 + / 8 +");
+        System.out.println(c.math("5 6 7 + / 8 +"));
     }
 }
